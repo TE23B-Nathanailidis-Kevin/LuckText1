61dcba97e46c1c8376f657a2be757ae1018f08d0
Kevin Nathanailidis <kevin.nathanailidis@elev.ga.ntig.se>
2024-09-24 13:06:38 +0200

    first commit

---
+++ LuckText/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Write your name");
+string name = Console.ReadLine();
+Console.WriteLine("Favorite Weapon");
+String Weapon = Console.ReadLine();
+Console.WriteLine("A natonality");
+string nationality = Console.ReadLine();
+Console.WriteLine($"{name} beats {nationality} with a {Weapon}");
+Console.ReadLine();
